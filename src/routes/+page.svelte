ec26e33e0d4c739f79706757e3e385db77f510e8
Tom Planche <tomplanche@proton.me>
2025-07-20 11:58:21 +0200

    [14] (feat on main) Fixed delete btn logic + fixed ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -102,30 +102,30 @@
       <div class="guess-container">
         <WordGuess key={index} bind:word={guesses[index]}/>
 
-        {#if index !== 0 && guesses.length < 1}
-          <button
-              class="delete-guess-btn"
-              onclick={() => deleteGuess(index)}
-              aria-label="Delete guess"
+        <button
+            class="delete-guess-btn"
+            class:invisible={index === 0 || guesses.length <= 1}
+            onclick={() => deleteGuess(index)}
+            aria-label="Delete guess"
+            disabled={index === 0 || guesses.length <= 1}
+        >
+          <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
           >
-            <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z"
-                  fill="currentColor"
-              />
-              <path d="M9 9H11V17H9V9Z" fill="currentColor"/>
-              <path d="M13 9H15V17H13V9Z" fill="currentColor"/>
-            </svg>
-          </button>
-        {/if}
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z"
+                fill="currentColor"
+            />
+            <path d="M9 9H11V17H9V9Z" fill="currentColor"/>
+            <path d="M13 9H15V17H13V9Z" fill="currentColor"/>
+          </svg>
+        </button>
       </div>
     {/each}
 
@@ -198,12 +198,17 @@
       border-radius: 8px;
       font-size: 1.5rem;
 
-      &:hover {
+      &:hover:not(.invisible) {
         background-color: #cb3837;
 
         svg {
           scale: 1.2;
         }
+      }
+
+      &.invisible {
+        opacity: 0;
+        pointer-events: none;
       }
 
       svg {
